b29e9bcf477382f66f5ec0bcfcabbff782950cd1
Vagmin Viswanathan <vagmin.viswanathan@gmail.com>
2026-01-27 06:36:40 -0500

    Add extra credit packages

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,17 @@
 import eslint from 'vite-plugin-eslint2';
 import { defineConfig } from "vite";
 import autoprefixer from 'autoprefixer';
+import tailwindcss from '@tailwindcss/postcss';
+import VitePluginFavicon from 'vite-plugin-favicon';
 
 export default defineConfig({
     plugins: [
         eslint(),
+        VitePluginFavicon('./src/img/friday_logo_v1.png'),
     ],
     css: {
         postcss: {
-            plugins: [autoprefixer()],
+            plugins: [tailwindcss(), autoprefixer()],
         }
     }
 })
