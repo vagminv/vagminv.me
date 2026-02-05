df0e61b2cb2ef651349933f1894413c840ce1871
Vagmin Viswanathan <vagmin.viswanathan@gmail.com>
2026-02-05 12:28:09 -0500

    Rename vite config to TypeScript

---
+++ vite.config.ts
@@ -0,0 +1,18 @@
+import eslint from 'vite-plugin-eslint2'
+import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite'
+import autoprefixer from 'autoprefixer'
+import tailwindcss from '@tailwindcss/postcss'
+
+export default defineConfig({
+  base: '/',
+  plugins: [
+    react(),
+    eslint(),
+  ],
+  css: {
+    postcss: {
+      plugins: [tailwindcss(), autoprefixer()],
+    },
+  },
+})
